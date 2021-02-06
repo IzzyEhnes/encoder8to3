a8aee9cf5749d554b61304df18748d2419140071
Izzy Ehnes <izzyehnes@gmail.com>
2021-02-06 15:45:30 -0800

    Feature: Add outputs

--- encoder8to3.v
+++ encoder8to3.v
@@ -23,4 +23,38 @@ module TestMod;
             $time, i0, i1, i2, i3, i4, i5, i6, i7, o2, o1, o0);
     end
 
+    initial begin
+        i0 = 1; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 1; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 1; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 1; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 1; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 1; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 1; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 1;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 1; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 1; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 1; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 1; i4 = 0; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 1; i5 = 0; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 1; i6 = 0; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 1; i7 = 0;   // initially 1000
+        #1;                                                               // wait 1 cycle
+        i0 = 0; i1 = 0; i2 = 0; i3 = 0; i4 = 0; i5 = 0; i6 = 0; i7 = 1;   // initially 1000
+    end
+
 endmodule
