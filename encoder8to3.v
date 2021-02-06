41af0ec2b1a6afc6eb80c022bd7413e9cb430c7d
Izzy Ehnes <izzyehnes@gmail.com>
2021-02-06 15:49:54 -0800

    Fix: Fix syntax error

--- encoder8to3.v
+++ encoder8to3.v
@@ -8,7 +8,7 @@ module EncoderMod(i0, i1, i2, i3, i4, i5, i6, i7, o2, o1, o0);
     or(o2, i4, i5, i6, i7);
     or(o1, i2, i3, i6, i7);
     or(o0, i1, i3, i5, i7);
-end module
+endmodule
 
 module TestMod;
     reg i0, i1, i2, i3, i4, i5, i6, i7;
