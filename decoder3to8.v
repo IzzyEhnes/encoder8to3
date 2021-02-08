b46c3e8567c428d5b7155ee0c7d5b275900dd494
Izzy Ehnes <izzyehnes@gmail.com>
2021-02-07 18:34:52 -0800

    Feature: Add TestMod

--- decoder3to8.v
+++ decoder3to8.v
@@ -18,5 +18,17 @@ module DecoderMod(i2, i1, i0, o0, o1, o2, o3, o4, o5, o6, o7);
     and(o5,     i2, not_i1,     i0);
     and(o6,     i2,     i1, not_i0);
     and(o7,     i2,     i1,     i0);
+endmodule
 
+module TestMod;
+    reg i2, i1, i0;
+    wire o0, o1, o2, o3, o4, o5, o6, o7;
+
+    DecoderMod my_decoder(i2, i1, i0, o0, o1, o2, o3, o4, o5, o6, o7);
+
+    initial begin
+        $display("Time  i2  i1  i0   o0  o1  o2  o3  o4  o5  o6  o7");
+        $display("----  ----------   ------------------------------");
+        $monitor("   %0d   %b   %b   %b   %b   %b   %b   %b   %b   %b   %b   %b", $time, i2, i1, i0, o0, o1, o2, o3, o4, o5, o6, o7);
+    end
 endmodule
